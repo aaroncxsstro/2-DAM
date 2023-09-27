435a4dd5647ba1314baeaeec33bd9430b103d010
Aaron Castro <aaroncasstroarias@gmail.com>
2023-09-27 08:19:20 +0200

    Clase 26/09/2023

---
+++ Desarrollo de interfaces/NetBeansProjects/Prueba/src/main/java/com/mycompany/prueba/PrimaryController.java
@@ -0,0 +1,19 @@
+package com.mycompany.prueba;
+
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class PrimaryController {
+
+    @FXML
+    private Button btnSaludar;
+    
+    @FXML
+    private TextField txtSaludo;
+    
+    public void saludar(ActionEvent event){
+        
+    }
+}
