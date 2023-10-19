3e5d607d5005644d2676f659e89314247eb1d33d
Aaron Castro <aaroncasstroarias@gmail.com>
2023-10-19 10:49:57 +0200

    Clase 19/10/2023

---
+++ Desarrollo de interfaces/NetBeansProjects/MaquinaCafe/src/main/java/com/mycompany/maquinacafe/Cafe.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.maquinacafe;
+
+ 
+public class Cafe {
+
+
+    String tipoCafe;
+    String tamañoCafe;
+    double precio;
+    int cantidad;
+
+    public String getTipoCafe() {
+        return tipoCafe;
+    }
+
+    public void setTipoCafe(String tipoCafe) {
+        this.tipoCafe = tipoCafe;
+    }
+
+    public String getTamañoCafe() {
+        return tamañoCafe;
+    }
+
+    public void setTamañoCafe(String tamañoCafe) {
+        this.tamañoCafe = tamañoCafe;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Cafe(String tipoCafe, String tamañoCafe, int cantidad) {
+        this.tipoCafe = tipoCafe;
+        this.tamañoCafe = tamañoCafe;
+        this.cantidad = cantidad;
+    }
+    
+    public void calcularPrecio(){
+        switch (this.tamañoCafe){
+            case "Pequeño": this.precio=0.50*this.cantidad;
+            case "Mediano": this.precio=1.0*this.cantidad;
+            case "Grande": this.precio=1.50*this.cantidad;
+                
+    }
+    }
+    
+}
---
+++ Desarrollo de interfaces/NetBeansProjects/MaquinaCafe/src/main/java/com/mycompany/maquinacafe/Cafeteria.java
@@ -0,0 +1,189 @@
+package com.mycompany.maquinacafe;
+
+import java.io.IOException;
+import java.util.Optional;
+import javafx.fxml.FXML;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+public class Cafeteria {
+   
+       @FXML
+    private TableView<Cafe> tabla;
+       
+       @FXML
+    private TableColumn<Cafe, String> tipoColumn;
+
+    @FXML
+    private TableColumn<Cafe, String> tamañoColumn;
+
+    @FXML
+    private TableColumn<Cafe, String> cantidadColumn;
+
+    @FXML
+    private TableColumn<Cafe, String> precioColumn;    
+
+    @FXML
+    private TextField fieldCantidad;
+        
+    @FXML
+    private Label almacenSaldo;
+        
+     @FXML
+    private RadioButton cortado;
+
+    @FXML
+    private RadioButton latte;
+
+    @FXML
+    private RadioButton capuccino;
+
+    @FXML
+    private Button btnSaldo;
+
+    @FXML
+    private TextField fieldSaldo;
+
+    @FXML
+    private ComboBox<String> cbx;
+
+    @FXML
+private void mostrarAlertaError(String error) {
+    Alert alert = new Alert(Alert.AlertType.ERROR);
+    alert.setHeaderText(null);
+    alert.setTitle("Error");
+    alert.setContentText(error);
+    alert.showAndWait();
+}
+
+    public void initialize() {
+        ObservableList<String> options = FXCollections.observableArrayList("Pequeño", "Mediano", "Grande");
+        cbx.setItems(options);
+        
+    tipoColumn.setCellValueFactory(new PropertyValueFactory<>("tipoCafe"));
+    tamañoColumn.setCellValueFactory(new PropertyValueFactory<>("tamañoCafe"));
+    precioColumn.setCellValueFactory(new PropertyValueFactory<>("precio"));
+    cantidadColumn.setCellValueFactory(new PropertyValueFactory<>("cantidad"));
+}
+    
+    @FXML
+     void añadirSaldo (ActionEvent event) {
+        double saldoIntroducido=0;
+         String saldoIntroducidoText = fieldSaldo.getText();
+         
+     try {
+        saldoIntroducido = Double.parseDouble(saldoIntroducidoText);
+            if(saldoIntroducido<=0){
+         mostrarAlertaError("El saldo introducido debe ser mayor que 0");
+            }else{
+                
+    String saldoActualText=almacenSaldo.getText().substring(0,almacenSaldo.getText().length()-1);
+    
+    double saldoActual = Double.parseDouble(saldoActualText);
+    saldoActual = saldoActual + saldoIntroducido;
+    
+    almacenSaldo.setText(String.valueOf(saldoActual)+"€");
+            
+    
+     }
+    } catch (NumberFormatException e) {
+       mostrarAlertaError("Introduce un caracter válido ");
+    } 
+ 
+     }
+     
+     @FXML
+    public void pedirCafe (ActionEvent event){
+        
+        ToggleGroup toggleGroup = new ToggleGroup();
+        cortado.setToggleGroup(toggleGroup);
+        latte.setToggleGroup(toggleGroup);
+        capuccino.setToggleGroup(toggleGroup);
+        
+       if (toggleGroup.getSelectedToggle() != null) {
+           
+ ToggleButton selectedToggle = (ToggleButton) toggleGroup.getSelectedToggle();
+String tipo = selectedToggle.getText();
+
+if(cbx.getValue()!=null){
+    
+String tamaño = cbx.getValue();
+
+try{
+    
+    String cantidadText = fieldCantidad.getText();
+    int cantidad = Integer.parseInt(cantidadText);
+
+          if(cantidad<=0){
+         mostrarAlertaError("La cantidad introducida debe ser mayor que cero");
+            }else{
+            
+              Cafe c1 = new Cafe (tipo, tamaño, cantidad);
+              c1.calcularPrecio();
+              double saldoNum=0;
+              String saldoText = fieldSaldo.getText();
+              if(saldoText!=null){
+              saldoNum = Double.parseDouble(saldoText);
+              }
+              if(saldoNum<c1.getPrecio()){
+         mostrarAlertaError("No hay saldo suficiente");
+              }else {
+              
+              tabla.getItems().add(c1);
+              
+     String saldoActualText=almacenSaldo.getText().substring(0,almacenSaldo.getText().length()-1);
+    double saldoActual = Double.parseDouble(saldoActualText);
+    saldoActual = saldoActual - saldoNum;
+    
+    almacenSaldo.setText(String.valueOf(saldoActual)+"€");
+              }
+              
+          }
+              } catch (NumberFormatException e) {
+       mostrarAlertaError("Introduce un caracter válido1");
+    } 
+            
+}else{
+mostrarAlertaError("Debes seleccionar el tamaño del café");
+}
+} else {
+           
+mostrarAlertaError("Debes seleccionar el tipo de café");
+
+} 
+       
+    }
+    
+    public void eliminarCafeSeleccionado(ActionEvent event) {
+    Cafe cafeSeleccionado = tabla.getSelectionModel().getSelectedItem();
+
+    if (cafeSeleccionado != null) {
+        Alert alert = new Alert(AlertType.CONFIRMATION);
+        alert.setTitle("Confirmación");
+        alert.setHeaderText(null);
+        alert.setContentText("¿Estás seguro de que deseas eliminar este café?");
+        
+        Optional<ButtonType> resultado = alert.showAndWait();
+
+        if (resultado.isPresent() && resultado.get() == ButtonType.OK) {
+            tabla.getItems().remove(cafeSeleccionado);
+        }
+    } else {
+        mostrarAlertaError("Selecciona un café para eliminar.");
+    }
+}
+    }
